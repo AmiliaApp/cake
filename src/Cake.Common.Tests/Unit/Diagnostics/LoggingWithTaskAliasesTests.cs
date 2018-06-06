73e73e1f5de5cffc411bf126b9a3e80dda1259b8
Michael Broadbent <michael.broadbent@amilia.com>
2018-06-06 14:20:58 -0400

    LoggingWithTask aliases

---
+++ src/Cake.Common.Tests/Unit/Diagnostics/LoggingWithTaskAliasesTests.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cake.Common.Diagnostics;
+using Cake.Core;
+using Cake.Core.Diagnostics;
+using NSubstitute;
+using Xunit;
+
+namespace Cake.Common.Tests.Unit.Diagnostics
+{
+    public class LoggingWithTaskAliasesTests
+    {
+        public sealed class TheErrorPrependTaskName
+        {
+            [Fact]
+            public void Should_Prepend_Task_Name_In_Angle_Brackets()
+            {
+                // Given
+                var taskName = "taskName";
+                var value = "thing to log";
+
+                // When
+                var prependedWithTaskName = LoggingWithTaskAliases.PrependTaskName(taskName, value);
+
+                // Then
+                Assert.Equal($"<{taskName}> {value}", prependedWithTaskName);
+            }
+        }
+
+        public sealed class TheErrorMethod
+        {
+            [Fact]
+            public void Should_Write_Error_String_Value_To_Log()
+            {
+                // Given
+                var context = Substitute.For<ICakeContext>();
+                context.Log.Returns(Substitute.For<ICakeLog>());
+
+                var taskName = "TestTask";
+                const string value = "Hello {0}";
+                var prependedWithTaskName = LoggingWithTaskAliases.PrependTaskName(taskName, value);
+
+                // When
+                context.ErrorWithTask(taskName, value);
+
+                // Then
+                context.Log.Received(1).Write(Verbosity.Quiet, LogLevel.Error, "{0}", prependedWithTaskName);
+            }
+        }
+
+        public sealed class TheWarningMethod
+        {
+            [Fact]
+            public void Should_Write_Warning_String_Value_To_Log()
+            {
+                // Given
+                var context = Substitute.For<ICakeContext>();
+                context.Log.Returns(Substitute.For<ICakeLog>());
+
+                var taskName = "TestTask";
+                const string value = "Hello {0}";
+                var prependedWithTaskName = LoggingWithTaskAliases.PrependTaskName(taskName, value);
+
+                // When
+                context.WarningWithTask(taskName, value);
+
+                // Then
+                context.Log.Received(1).Write(Verbosity.Minimal, LogLevel.Warning, "{0}", prependedWithTaskName);
+            }
+        }
+
+        public sealed class TheInformationMethod
+        {
+            [Fact]
+            public void Should_Write_Information_String_Value_To_Log()
+            {
+                // Given
+                var context = Substitute.For<ICakeContext>();
+                context.Log.Returns(Substitute.For<ICakeLog>());
+
+                var taskName = "TestTask";
+                const string value = "Hello {0}";
+                var prependedWithTaskName = LoggingWithTaskAliases.PrependTaskName(taskName, value);
+
+                // When
+                context.InformationWithTask(taskName, value);
+
+                // Then
+                context.Log.Received(1).Write(Verbosity.Normal, LogLevel.Information, "{0}", prependedWithTaskName);
+            }
+        }
+
+        public sealed class TheVerboseMethod
+        {
+            [Fact]
+            public void Should_Write_Verbose_String_Value_To_Log()
+            {
+                // Given
+                var context = Substitute.For<ICakeContext>();
+                context.Log.Returns(Substitute.For<ICakeLog>());
+
+                var taskName = "TestTask";
+                const string value = "Hello {0}";
+                var prependedWithTaskName = LoggingWithTaskAliases.PrependTaskName(taskName, value);
+
+                // When
+                context.VerboseWithTask(taskName, value);
+
+                // Then
+                context.Log.Received(1).Write(Verbosity.Verbose, LogLevel.Verbose, "{0}", prependedWithTaskName);
+            }
+        }
+
+        public sealed class TheDebugMethod
+        {
+            [Fact]
+            public void Should_Write_Debug_String_Value_To_Log()
+            {
+                // Given
+                var context = Substitute.For<ICakeContext>();
+                context.Log.Returns(Substitute.For<ICakeLog>());
+
+                var taskName = "TestTask";
+                const string value = "Hello {0}";
+                var prependedWithTaskName = LoggingWithTaskAliases.PrependTaskName(taskName, value);
+
+                // When
+                context.DebugWithTask(taskName, value);
+
+                // Then
+                context.Log.Received(1).Write(Verbosity.Diagnostic, LogLevel.Debug, "{0}", prependedWithTaskName);
+            }
+        }
+    }
+}
---
+++ src/Cake.Common/Diagnostics/LoggingWithTaskAliases.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.Diagnostics;
+
+namespace Cake.Common.Diagnostics
+{
+    /// <summary>
+    /// Contains functionality related to logging including the current task name.
+    /// </summary>
+    public static class LoggingWithTaskAliases
+    {
+        /// <summary>
+        /// Writes an error message to the log using the specified string value.
+        /// </summary>
+        /// <param name="context">the context.</param>
+        /// <param name="taskName">The task name.</param>
+        /// <param name="value">The value.</param>
+        /// <example>
+        /// <code>
+        /// ErrorWithTask("{taskName}", "{string}");
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Error")]
+        public static void ErrorWithTask(this ICakeContext context, string taskName, string value)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            context.Log.Error(PrependTaskName(taskName, value));
+        }
+
+        /// <summary>
+        /// Writes an warning message to the log using the specified string value.
+        /// </summary>
+        /// <param name="context">the context.</param>
+        /// <param name="taskName">The task name.</param>
+        /// <param name="value">The value.</param>
+        /// <example>
+        /// <code>
+        /// WarningWithTask("{taskName}", "{string}");
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Warning")]
+        public static void WarningWithTask(this ICakeContext context, string taskName, string value)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            context.Log.Warning(PrependTaskName(taskName, value));
+        }
+
+        /// <summary>
+        /// Writes an informational message to the log using the specified string value.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="taskName">The task name.</param>
+        /// <param name="value">The value.</param>
+        /// <example>
+        /// <code>
+        /// InformationWithTask("{taskName}", "{string}");
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Information")]
+        public static void InformationWithTask(this ICakeContext context, string taskName, string value)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            context.Log.Information(PrependTaskName(taskName, value));
+        }
+
+        /// <summary>
+        /// Writes an verbose message to the log using the specified string value.
+        /// </summary>
+        /// <param name="context">the context.</param>
+        /// <param name="taskName">The task name.</param>
+        /// <param name="value">The value.</param>
+        /// <example>
+        /// <code>
+        /// VerboseWithTask("{taskName}", "{string}");
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Verbose")]
+        public static void VerboseWithTask(this ICakeContext context, string taskName, string value)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            context.Log.Verbose(PrependTaskName(taskName, value));
+        }
+
+        /// <summary>
+        /// Writes an debug message to the log using the specified string value.
+        /// </summary>
+        /// <param name="context">the context.</param>
+        /// <param name="taskName">The task name.</param>
+        /// <param name="value">The value.</param>
+        /// <example>
+        /// <code>
+        /// DebugWithTask("{taskName}", "{string}");
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Debug")]
+        public static void DebugWithTask(this ICakeContext context, string taskName, string value)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            context.Log.Debug(PrependTaskName(taskName, value));
+        }
+
+        /// <summary>
+        /// Format the given <c>value</c> with the <c>taskName</c> prepended in angle brackets.
+        /// </summary>
+        /// <param name="taskName">The taskname to prepend</param>
+        /// <param name="value">The log to write</param>
+        /// <returns>A formatted string with the <c>taskName</c> prepended in angle brackets</returns>
+        public static string PrependTaskName(string taskName, string value)
+        {
+            return $"<{taskName}> {value}";
+        }
+    }
+}
